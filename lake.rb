ea7e9121d29b5094c3ad57ead03a5586abd03831
Adler <nkj20932@hotmail.com>
2015-10-23 11:41:54 +0800

    update dependencies

--- lake.rb
+++ lake.rb
@@ -4,6 +4,7 @@ class Lake < Formula
   url "https://github.com/adlerhsieh/lake/archive/0.1.2.tar.gz"
   sha256 "79653d97174771551623cde06b52827d64c12c8ec8c1c328417adb270ede80b1"
   depends_on :x11
+  depends_on "bdw-gc"
   def install
     bin.install "lake"
     # system "./configure", "--disable-debug",
