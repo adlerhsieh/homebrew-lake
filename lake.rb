b19dbf4c73975193f1eedb7feed2fa63ed96c7b7
Adler <nkj20932@hotmail.com>
2015-10-23 11:27:04 +0800

    update key

--- lake.rb
+++ lake.rb
@@ -2,7 +2,7 @@ class Lake < Formula
   desc "rake-inspired tool in Crystal for tasks management"
   homepage "https://github.com/adlerhsieh/lake"
   url "https://github.com/adlerhsieh/lake/archive/0.1.2.tar.gz"
-  sha256 "e6a611f65b5646928d39c7a774b3ad1ec342e85b68986e401f8bd854e05187f3"
+  sha256 "79653d97174771551623cde06b52827d64c12c8ec8c1c328417adb270ede80b1"
   depends_on :x11
   def install
     bin.install "lake"
