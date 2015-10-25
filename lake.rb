c0b71f4f86cb42b11a1c58bfe6508f33e78e1a7a
Adler <nkj20932@hotmail.com>
2015-10-26 00:09:13 +0800

    update formula

--- lake.rb
+++ lake.rb
@@ -1,7 +1,7 @@
 class Lake < Formula
   desc "rake-inspired tool in Crystal for tasks management"
   homepage "https://github.com/adlerhsieh/lake"
-  url "https://github.com/adlerhsieh/lake/archive/0.1.3.tar.gz"
+  url "https://github.com/adlerhsieh/lake/archive/0.1.4.tar.gz"
   sha256 "5edd863bcfa0284eac54f047e7dfe7380f626d165a55e519925084950848da9b"
   depends_on :x11
   depends_on "bdw-gc"
