7aefcf89d5a4791c7f0d5113dfc0977a84bbd51e
Adler <nkj20932@hotmail.com>
2015-10-25 10:25:30 +0800

    add crystal-lang dependencies

--- lake.rb
+++ lake.rb
@@ -5,6 +5,7 @@ class Lake < Formula
   sha256 "79653d97174771551623cde06b52827d64c12c8ec8c1c328417adb270ede80b1"
   depends_on :x11
   depends_on "bdw-gc"
+  depends_on "crystal-lang"
   def install
     bin.install "lake"
     # system "./configure", "--disable-debug",
